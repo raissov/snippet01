c26a5ce4ec0a15e651290ce769be82b7a6ea6f70
Beknur Raissov <brown.robert@bk.ru>
2021-01-07 21:08:09 +0600

    using Latest method from snippets.go in the home method

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -3,38 +3,46 @@ package main
 import (
 	"errors"
 	"fmt"
-	"html/template"
 	"net/http"
 	"raissov/snippetbox/pkg/models"
 	"strconv"
 )
 
-func (app *application) home(w http.ResponseWriter, r *http.Request)  {
-	if r.URL.Path != "/"{
+func (app *application) home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
 		app.notFound(w)
 		return
 	}
+	s, err := app.snippets.Latest()
 
-	files := []string{
-		"./ui/html/home.page.tmpl",
-		"./ui/html/base.layout.tmpl",
-		"./ui/html/footer.partial.tmpl",
-	}
-
-	ts, err := template.ParseFiles(files...)
 	if err != nil {
 		app.serverError(w, err)
 		return
 	}
-
-	err = ts.Execute(w, nil)
-	if err != nil {
-		app.serverError(w, err)
+	for _, snippet := range s {
+		fmt.Fprintf(w, "%v\n", snippet)
 	}
+
+	//files := []string{
+	//	"./ui/html/home.page.tmpl",
+	//	"./ui/html/base.layout.tmpl",
+	//	"./ui/html/footer.partial.tmpl",
+	//}
+	//
+	//ts, err := template.ParseFiles(files...)
+	//if err != nil {
+	//	app.serverError(w, err)
+	//	return
+	//}
+	//
+	//err = ts.Execute(w, nil)
+	//if err != nil {
+	//	app.serverError(w, err)
+	//}
 
 }
 
-func (app *application) showSnippet(w http.ResponseWriter, r *http.Request)  {
+func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
 	if err != nil || id < 1 {
 		app.notFound(w)
@@ -42,9 +50,9 @@ func (app *application) showSnippet(w http.ResponseWriter, r *http.Request)  {
 	}
 	s, err := app.snippets.Get(id)
 	if err != nil {
-		if errors.Is(err, models.ErrNoRecord){
+		if errors.Is(err, models.ErrNoRecord) {
 			app.notFound(w)
-		}else {
+		} else {
 			app.serverError(w, err)
 		}
 		return
@@ -53,11 +61,11 @@ func (app *application) showSnippet(w http.ResponseWriter, r *http.Request)  {
 	fmt.Fprintf(w, "%v", s)
 }
 
-func (app *application) createSnippet(w http.ResponseWriter, r *http.Request)  {
+func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
-			w.Header().Set("Allow", http.MethodPost)
-			app.clientError(w, http.StatusMethodNotAllowed)
-			return
+		w.Header().Set("Allow", http.MethodPost)
+		app.clientError(w, http.StatusMethodNotAllowed)
+		return
 	}
 
 	title := "0 snail"
@@ -71,6 +79,6 @@ func (app *application) createSnippet(w http.ResponseWriter, r *http.Request)  {
 		return
 	}
 
-	http.Redirect(w, r, fmt.Sprintf("/snippet=%id",id), http.StatusSeeOther)
-	w.Write([]byte ("Create a new snippet..."))
+	http.Redirect(w, r, fmt.Sprintf("/snippet=%id", id), http.StatusSeeOther)
+	w.Write([]byte("Create a new snippet..."))
 }
