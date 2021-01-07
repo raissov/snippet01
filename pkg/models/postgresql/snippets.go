d767ae70d28bdc44ad65697086d1e6ac97c3b660
Beknur Raissov <brown.robert@bk.ru>
2021-01-07 21:00:22 +0600

    some changes

--- pkg/models/postgresql/snippets.go
+++ pkg/models/postgresql/snippets.go
@@ -43,3 +43,30 @@ func (m *SnippetModel) Get(id int) ( *models.Snippet, error)  {
 
 }
 
+func (m *SnippetModel) Latest() ([]*models.Snippet, error)  {
+	stmt := `SELECT id, title, content, created, expires, FROM snippets WHERE expires > now() ORDER BY created DESC LIMIT 10`
+
+	rows, err := m.DB.Query(context.Background(), stmt)
+
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	snippets := []*models.Snippet
+
+	for rows.Next(){
+		s := &models.Snippet{}
+		err = rows.Scan(&s.ID, &s.Title, s.Content, s.Created, s.Expires)
+		if err != nil {
+			return nil, err
+		}
+		snippets = append(snippets, s)
+	}
+	if err = rows.Err();
+	err != nil{
+		return nil, err
+	}
+	return snippets, nil
+}
+
