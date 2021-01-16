5e63cb8022bf090b5426da2049409eaf962d3dac
raissov <brown.robert@bk.ru>
2021-01-16 15:15:29 +0600

    added: making url compact

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -9,10 +9,6 @@ import (
 )
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
-		app.notFound(w)
-		return
-	}
 
 	s, err := app.snippets.Latest()
 	if err != nil {
@@ -25,7 +21,7 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
-	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	id, err := strconv.Atoi(r.URL.Query().Get(":id"))
 	if err != nil || id < 1 {
 		app.notFound(w)
 		return
@@ -44,12 +40,11 @@ func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 	})
 }
 
+func (app *application) createSnippetForm(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Creating a new snippet..."))
+}
+
 func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		w.Header().Set("Allow", http.MethodPost)
-		app.clientError(w, http.StatusMethodNotAllowed)
-		return
-	}
 
 	title := "0 snail"
 	content := "O snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n– Kobayashi Issa"
@@ -62,6 +57,5 @@ func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	http.Redirect(w, r, fmt.Sprintf("/snippet=%id", id), http.StatusSeeOther)
-	w.Write([]byte("Create a new snippet..."))
+	http.Redirect(w, r, fmt.Sprintf("/snippet/%d", id), http.StatusSeeOther)
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module raissov/snippetbox
 go 1.15
 
 require (
+	github.com/bmizerany/pat v0.0.0-20170815010413-6226ea591a40
 	github.com/jackc/pgx/v4 v4.10.1
-	github.com/justinas/alice v1.2.0 // indirect
+	github.com/justinas/alice v1.2.0
 )
