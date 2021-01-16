93bc6dcfbad3a539edb7a87b2714b44f5172c6fd
raissov <brown.robert@bk.ru>
2021-01-16 12:00:44 +0600

    fixed errors in the snippets.go

--- pkg/models/postgresql/snippets.go
+++ pkg/models/postgresql/snippets.go
@@ -9,31 +9,28 @@ import (
 )
 
 type SnippetModel struct {
-	DB *pgxpool.Conn
+	//DB *sql.DB
+	DB *pgxpool.Pool
 }
 
 func (m *SnippetModel) Insert(title, content, expires string) (int, error) {
-	stmt := `INSERT INTO snippets (title, content, created, expires) VALUES($1, $2, now(), now() + make_interval(days =>$3)) RETURNING id`
+	stmt := "INSERT INTO snippets (title, content, created, expires) VALUES($1, $2, NOW(), NOW() + make_interval(days => $3)) returning id"
 
-	var id int
+	id := 0
+
 	err := m.DB.QueryRow(context.Background(), stmt, title, content, expires).Scan(&id)
-
 	if err != nil {
 		return 0, err
 	}
 
 	return id, nil
-
 }
 
+// This will return a specific snippet based on its id.
 func (m *SnippetModel) Get(id int) (*models.Snippet, error) {
-	stmt := `SELECT id, title, content, created, expires FROM snippets WHERE expires > now() AND id = $1`
-
-	row := m.DB.QueryRow(context.Background(), stmt, id)
-
-	sm := &models.Snippet{}
-
-	err := row.Scan(&sm.ID, &sm.Title, &sm.Content, &sm.Created, sm.Expires)
+	s := &models.Snippet{}
+	stmt := "SELECT id, title, content, created, expires FROM snippets WHERE expires > NOW() AND id = $1"
+	err := m.DB.QueryRow(context.Background(), stmt, id).Scan(&s.ID, &s.Title, &s.Content, &s.Created, &s.Expires)
 
 	if err != nil {
 		if errors.Is(err, sql.ErrNoRows) {
@@ -41,34 +38,35 @@ func (m *SnippetModel) Get(id int) (*models.Snippet, error) {
 		} else {
 			return nil, err
 		}
-
 	}
-	return sm, nil
-
+	return s, nil
 }
 
+// This will return the 10 most recently created snippets.
 func (m *SnippetModel) Latest() ([]*models.Snippet, error) {
-	stmt := `SELECT id, title, content, created, expires, FROM snippets WHERE expires > now() ORDER BY created DESC LIMIT 10`
-
+	stmt := `SELECT id, title, content, created, expires FROM snippets
+			WHERE expires > NOW() ORDER BY created DESC LIMIT 10`
 	rows, err := m.DB.Query(context.Background(), stmt)
-
 	if err != nil {
 		return nil, err
 	}
+
 	defer rows.Close()
 
-	snippets := []*models.Snippet{}
+	var snippets []*models.Snippet
 
 	for rows.Next() {
 		s := &models.Snippet{}
-		err = rows.Scan(&s.ID, &s.Title, s.Content, s.Created, s.Expires)
+		err = rows.Scan(&s.ID, &s.Title, &s.Content, &s.Created, &s.Expires)
 		if err != nil {
 			return nil, err
 		}
 		snippets = append(snippets, s)
 	}
+
 	if err = rows.Err(); err != nil {
 		return nil, err
 	}
+
 	return snippets, nil
 }
